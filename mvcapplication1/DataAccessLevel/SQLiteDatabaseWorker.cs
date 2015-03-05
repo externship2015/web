15d9dd1119951681b7381cf62283bf46073d7040
Leonid <leon99_21@mail.ru>
2015-03-05 22:14:29 +0300

    alert

--- mvcapplication1/DataAccessLevel/SQLiteDatabaseWorker.cs
+++ mvcapplication1/DataAccessLevel/SQLiteDatabaseWorker.cs
@@ -34,7 +34,7 @@ namespace TheTime.DataAccessLevel
             string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Database.db";
            // string path = @"D:\DataBase.db";
 
-           // string path = @"C:\C#\Application\SimSoft\web\mvcapplication1\Database.db";
+         //   string path = @"C:\C#\Application\SimSoft\web\mvcapplication1\Database.db";
          //   string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Database.db";
 
             m_dbConnection = new SQLiteConnection(@"Data Source=" + path + ";Version=3;datetimeformat=CurrentCulture");
